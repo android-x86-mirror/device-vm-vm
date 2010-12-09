9c3923229c1dfb973b29a0c9b6aa6971650ae50b
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-12-09 18:40:33 +0800

    specify sdcard in cmdline instead of using vold.fstab

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,6 +4,4 @@ TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/vm_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/vm_defconfig
 
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab
-
 include $(GENERIC_X86_ANDROID_MK)
