5ef3eda222a8457820de6d12dd9114e45b3bb9e6
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-01-24 00:40:03 +0800

    update for gingerbread-x86

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 LOCAL_FIRMWARES :=
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/vm_info
-TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/vm_defconfig
 
 include $(GENERIC_X86_ANDROID_MK)
